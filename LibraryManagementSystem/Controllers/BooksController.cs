1a0fdf63ab5705803f512080001ea7353af12dd3
Nirmada <nirmada0504@gmail.com>
2025-01-08 22:54:47 +0530

    Edit and Delete books

--- LibraryManagementSystem/Controllers/BooksController.cs
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -35,5 +35,66 @@ namespace LibraryManagementSystem.Controllers
             }
             return View(book);
         }
+
+        // GET: Books/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(book);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+               _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
